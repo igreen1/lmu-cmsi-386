e460df73863cbe8f7f93833314cab82dde3fe040
igreen1 <iangreen2.00@gmail.com>
2020-09-08 18:14:27 -0700

    Finished exercise 3

--- homework1/src/exercises.js
+++ homework1/src/exercises.js
@@ -44,11 +44,11 @@ A function that accepts a string and returns a new string equal to the initial s
     whitespace removed and then with the ith character (1-based) repeated i times. 
 
 Input: string 
-Output: string with no white space and repeated characters based on position
+Output: input string with no white space and repeated characters based on position
 */
 function stretched(inputString){
 
-    var manipString = ""     //String to hold the manipulated string. 
+    var outputString = ""     //String to hold the manipulated string. 
                         //  Makes indexing way easier
     var stripped        //Holds the inputString without whitespace (to preserve input variable)
     
@@ -62,17 +62,54 @@ function stretched(inputString){
     //add character based on location
     for(var i = 0; i < stripped.length; i++)
     {
-        manipString += stripped[i].repeat(i+1)
+        //Take each element, repeat is the amount of times as its location starting at 1
+        //  then append it to the output string
+        outputString += stripped[i].repeat(i+1)
     }
 
-    return manipString
+    return outputString
 
 }
 
 /*
-A function that randomly permutes a string. What does random mean? It means that each time you call the function for a given argument, all possible permutations are equally likely. Random is not the same as arbitrary. 
+A function that randomly permutes a string. What does random mean? It means that each time you 
+    call the function for a given argument, all possible permutations are equally likely. 
+    Random is not the same as arbitrary. 
+
+Input: string
+Output: input string permuted randomly
 */
-function scramble(x){
+function scramble(inputString){
+
+    //input validation
+    if(inputString.length <= 0){
+        return inputString
+    }
+
+    var outputString = ''                       //holds the output to return
+    var inputAsArray
+    inputAsArray = inputString.split("")    //protects input and makes easier to manipulate
+
+    //While there are characters unassigned,
+    //  1. pick a random character from input
+    //  2. add it to the output string
+    //  3. remove it from input string
+    var j
+    while (inputAsArray.length != 0){
+        
+        //this command is a bit of a doozy haha so i split it up
+        //first, find the index to copy (random index from iAA)
+        j = Math.floor(Math.random() * inputAsArray.length)
+        
+        //now remove that random element from inputAsArray
+        //this will return an array with 1 element as a string
+        //so derefence the array with .toString()
+        outputString += inputAsArray.splice(j,1).toString()
+
+
+    }
+
+    return outputString
 
 }
 
