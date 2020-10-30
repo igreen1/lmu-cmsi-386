58cd59be558c8390fb5cf3d963870f5c6f357b70
Veronica <veronicabp@gmail.com>
2020-10-29 19:37:12 -0700

    create pseudo code

---
+++ homework4/exercises.swift
@@ -0,0 +1,32 @@
+struct Result {
+
+}
+
+func change(amount: Int) -> Result {
+    var result: Result()
+    return result
+}
+
+func stretched(s: String) -> String {
+    return
+}
+
+func mapThenUnique () -> {
+
+}
+
+func powers() -> {
+
+}
+
+func say(text: String) -> Sayer {
+    return
+}
+
+func twice() -> {
+
+}
+
+func uppercasedFirst() -> {
+    
+}
