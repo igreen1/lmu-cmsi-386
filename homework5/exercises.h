4035b8a63899bef54ca04bbd067380482e890118
Veronica <veronicabp@gmail.com>
2020-11-19 19:18:22 -0800

    start to create functions

---
+++ homework5/exercises.h
@@ -0,0 +1,33 @@
+#include "./exercises.h"
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+Sayer say(string s) {
+
+}
+
+vector<pair<list<string>, vector<pair<string, int>>>> sorted_word_counts(list<string>) {
+
+}
+
+class Queue {
+
+	public:
+		void enqueue(String s) {
+
+		}
+	public:
+		string dequeue() {
+
+		}
+	public:
+		int get_size() {
+
+		}
+}
+
