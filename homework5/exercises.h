b59a0ddea7edb6a75090f270d60a416ae281b254
Booker-M <bookerjmartin@gmail.com>
2020-12-02 21:00:18 -0800

    Brought back Ian's move assignments

--- homework5/exercises.h
+++ homework5/exercises.h
@@ -54,8 +54,42 @@ public:
 	Queue(Queue&) = delete;
 	Queue& operator=(Queue&) = delete;
 
-	Queue(Queue&&) = default;
-	Queue& operator=(Queue&&) = default;
+	Queue(Queue&& other) {
+		this->head = nullptr;
+		this->tail = nullptr;
+		this->size = 0;
+		if (other.head != nullptr) {
+			this->head = new Node(other.head->data);
+			this->size++;
+			Node* otherNode = other.head;
+			Node* node = this->head;
+			while (otherNode->next != nullptr) {
+				otherNode = otherNode->next;
+				node->next = new Node(otherNode->data);
+				this->size++;
+				node = node->next;
+			}
+		}
+	}
+
+	Queue& operator=(Queue&& other) {
+		this->head = nullptr;
+		this->tail = nullptr;
+		this->size = 0;
+		if (other.head != nullptr) {
+			this->head = new Node(other.head->data);
+			this->size++;
+			Node* otherNode = other.head,
+				* node = this->head;
+			while (otherNode->next != nullptr) {
+				otherNode = otherNode->next;
+				node->next = new Node(otherNode->data);
+				this->size++;
+				node = node->next;
+			}
+		}
+		return *this;
+	}
 
 	void enqueue(T data) {
 		if (this->tail != nullptr) { // size > 1
